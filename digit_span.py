ef6e2a901040badb4506423867cac795656c46f2
Yizhen-Wen <45116798+Yizhen-Wen@users.noreply.github.com>
2018-12-12 16:38:06 +0800

    Update digit_span.py

--- digit_span.py
+++ digit_span.py
@@ -4,24 +4,21 @@ import sys
 import random
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-def randomcolor():	#	随机生成颜色
-    colorArr = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-    color = ""
-    for i in range(6):
-        color += colorArr[random.randint(0,14)]
-    return "#"+color
+interval = 500	#	set the interval value for the number display 
+path_name = './log.txt'
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.isTimeStart=False
-        self.level = 3	#	由三个数字开始
+        self.current_timestr = '00:00:00'
+        self.level = 3	#	begin with 3 numbers
         self.setupUI()
 
 
     def setupUI(self):
         
-        self.timer = QtCore.QTimer()
+        self.timer = QtCore.QTimer()    #   create a timer
         self.timeClock = QtCore.QTime()
         self.timer.timeout.connect(self.addtime)
 
@@ -29,7 +26,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.main_layout = QtWidgets.QHBoxLayout()
         self.main_widget.setLayout(self.main_layout)
         self.main_widget.setObjectName('main_widget')
-        self.setCentralWidget(self.main_widget) # 设置窗口主部件
+        self.setCentralWidget(self.main_widget) # set main window
 
         self.center_widget = QtWidgets.QWidget()
         self.center_layout = QtWidgets.QVBoxLayout()
@@ -54,11 +51,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.top_widget.setFixedSize(500,100)
 
         self.display_widget = QtWidgets.QLabel()
-        self.display_widget.setFixedSize(500,400)
+        self.display_widget.setFixedSize(1000,600)
         self.display_widget.setText('准备开始测试')
         self.display_widget.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
         self.input_widget = QtWidgets.QLineEdit()
-        self.input_widget.setFixedSize(500,60)
+        self.input_widget.setFixedSize(1000,60)
         self.input_widget.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
         self.input_widget.returnPressed.connect(self.check)
 
@@ -80,7 +77,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.bottom_layout.addStretch(1)
 
         self.center_layout.addWidget(self.top_widget)
-        self.center_layout.addStretch(1)
+        #self.center_layout.addStretch(1)
         self.center_layout.addWidget(self.display_widget)
         self.center_layout.addStretch(1)
         self.center_layout.addWidget(self.input_widget)
@@ -92,7 +89,6 @@ class MainWindow(QtWidgets.QMainWindow):
             QWidget#main_widget{
                 background:#554236;
                 border:none
-
             }
             QLabel{
                 background:white;
@@ -100,77 +96,87 @@ class MainWindow(QtWidgets.QMainWindow):
                 border-radius:20px;
                 color:#554236;
                 font-family: "Microsoft YaHei";
-                font-size:60px;
+                font-size:100px;
             }
             #QLabel:hover{background:#4d5e39;}
-            QLineEdit{background:white;border:none;border-radius:20px;color:#554236;font-family: "Segoe UI";font-size:20px;}
-            QPushButton#top_label{border:none;color:white;font-family: "Segoe UI";font-size:20px;}
-            QPushButton#level_label{border:none;color:#EBB471;font-family: "Segoe UI";font-size:25px;}
+            QLineEdit{
+            	background:white;border:none;border-radius:20px;color:#554236;font-family: "Segoe UI";font-size:20px;
+            }
+            QPushButton#top_label{
+            	border:none;color:white;font-family: "Segoe UI";font-size:20px;
+            }
+            QPushButton#level_label{
+            	border:none;color:#EBB471;font-family: "Segoe UI";font-size:25px;
+           	}
             QPushButton#bottom_button{
                 border:none;background:#724938;border-radius:10px;color:white;font-family: "Segoe UI";font-size:18px;
             }
             QPushButton#bottom_button:hover{background:#7d6c46;}
         ''')
 
-        self.resize(600, 800)  #   设置窗口大小
+        self.resize(1200, 1000)  #   设置窗口大小
         self.center()           #   设置窗口位于屏幕正中间
         self.setWindowTitle('Digit Span')      #   窗口标题
         self.setWindowOpacity(0.9)  #   设置窗口透明度
-
 
     def start(self):
         self.timestart()    # 总的时间显示
         self.num = 0
         self.index = 0
-        self.display_timer = QtCore.QTimer(self) #   控制每个数字显示一秒
-        self.display_timer.timeout.connect(self.operate) #计时结束调用operate()方法
-        self.display_timer.start(1000) #设置计时间隔并启动
+        self.display_timer = QtCore.QTimer(self)		 #	for number display time control
+        self.display_timer.timeout.connect(self.operate) #	connect to operate() to control
+        self.display_timer.start(interval)	#	set the interval
 
     def operate(self):
-        a = random.randint(0,9)
-        self.index += 1
-        print(self.num)
-        if self.index <= self.level:
-            self.num += a * (10**(self.index-1))
-            self.display_widget.setStyleSheet('QLabel{color:%s;font-size: 200px}'%randomcolor())	#	随机改变颜色以区分两次相同数字
-            self.display_widget.setText(str(a))
-        elif (self.index == self.level + 1):
-            self.display_widget.setStyleSheet('QLabel{color:#554236;font-size:60px}')
-            self.display_widget.setText('请反向输入\n刚刚的数字')
-            self.display_timer.stop()
+        with open(path_name,'a+') as f:
+	        a = random.randint(0,9)
+	        f.write(self.current_timestr+' 显示数字中 ')
+	        self.index += 1
+	        if self.index <= self.level:
+	            self.num += a * (10**(self.index-1))
+	            self.display_widget.setStyleSheet('QLabel{font-size:400px;}')
+	            self.display_widget.setText(str(a))
+	        elif (self.index == self.level + 1):
+	            self.display_widget.setStyleSheet('QLabel{font-size:100px;}')
+	            self.display_widget.setText('请反向输入\n刚刚的数字')
+	            f.write('\n'+self.current_timestr+' 提示输入\n')
+	            self.display_timer.stop()
 
     def check(self):
         inputnum = int(self.input_widget.text())
-        if inputnum == self.num:
-            self.level += 1
-            self.start()
-        else:
-            self.start()
-        self.top_label_2.setText('level '+str(self.level))
-        self.input_widget.clear()
+        with open(path_name,'a+') as f:
+	        if inputnum == self.num:
+	            f.write(self.current_timestr+' 判断：正确 level：'+str(self.level)+'\n')
+	            self.level += 1 
+	            self.start()
+	        else:
+	            f.write(self.current_timestr+' 判断：错误 level：'+str(self.level)+'\n')
+	            self.start()
+	        self.top_label_2.setText('level '+str(self.level))
+	        self.input_widget.clear()
 
 
     def stop(self):
         self.timestop()
 
-    #   显示计时器几个功能
-    def timestart(self):            #启动计时
+    #   set for timer
+    def timestart(self):            
         if not self.isTimeStart:
-            self.timeClock.setHMS(0,0,0)  #初始时设置时间为  00：00：00
-            self.timer.start(1000)         #启动定时器，定时器对象每隔一秒发射一个timeout信号
+            self.timeClock.setHMS(0,0,0)  	
+            self.timer.start(1000)         
         self.isTimeStart=True
 
-    def addtime(self):      #计时时间增一秒，并显示在QLable上
-        self.timeClock = self.timeClock.addMSecs(1000)   #时间增加一秒
-        self.label_time_val.setText(self.timeClock.toString("hh:mm:ss"))   #标签显示时间
+    def addtime(self):      
+        self.timeClock = self.timeClock.addMSecs(1000)   
+        self.current_timestr = self.timeClock.toString("hh:mm:ss")
+        self.label_time_val.setText(self.current_timestr)   # only for time label display
     
-    def timestop(self):    #停止计时
+    def timestop(self):    
         if self.isTimeStart:
             self.timer.stop()
             self.isTimeStart=False
         
-
-    #   使窗口显示在屏幕正中
+    #   set main window to the center
     def center(self):
         qr = self.frameGeometry()
         cp = QtWidgets.QDesktopWidget().availableGeometry().center()
